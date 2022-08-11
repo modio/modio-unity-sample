198179199f93a0f3efdb725420e3ab8e2f2363b9
stephenlucerne <48700284+stephenlucerne@users.noreply.github.com>
2022-08-11 15:23:54 +1000

    fixed static renderers to update on consecutive mod loads

--- Assets/Roguelike 2D/Scripts/mod.io Scripts/SpriteOverride.cs
+++ Assets/Roguelike 2D/Scripts/mod.io Scripts/SpriteOverride.cs
@@ -7,16 +7,28 @@
 /// </summary>
 public class SpriteOverride : MonoBehaviour
 {
+    // Use this boolean to let the override know there is no animator, so it can cache the starting sprite
+    [SerializeField] bool staticRenderer;
     [SerializeField] SpriteRenderer spriteRenderer;
 
+    Sprite originalSprite;
+    
     void Reset()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
+    void Awake()
+    {
+        if(staticRenderer)
+        {
+            originalSprite = spriteRenderer.sprite;
+        }
+    }
+
     void LateUpdate()
     {
-        Sprite sprite = SpriteOverrideManager.GetSprite(spriteRenderer.sprite);
+        Sprite sprite = SpriteOverrideManager.GetSprite(originalSprite ?? spriteRenderer.sprite);
         spriteRenderer.sprite = sprite;
     }
 }
